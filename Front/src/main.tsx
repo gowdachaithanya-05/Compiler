602d3097168116f9ed47db6c43ca86336c516757
Bhaskar Datta <34809663+bhaskardatta@users.noreply.github.com>
2025-01-04 19:19:25 +0530

    Add files via upload

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,6 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './styles/prism-custom.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Index from './pages/Index';
+import './index.css';
+import './styles/prism-custom.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+);
